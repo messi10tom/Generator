7c7d0c7c6a6129acd58ba731daf3a6d046718dbb
Messi Tom <91322610+messi10tom@users.noreply.github.com>
2021-10-24 21:36:23 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,22 +1,19 @@
-characters =  ['0', '1'] #['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
-#wordListFILE = open('wordlist.txt', 'w+')
-DIGITS = 16
-missed, combinations = Generator(characters, digits=DIGITS, shuffle=45)
-#missed = Generator(characters, minIndexNum, maxIndexNum, wordListFILE=wordListFILE, explicit=False)
-#wordListFILE.seek(0)
-#combinations = wordListFILE.read()
-#combinations = combinations.splitlines()
-gpw = len(combinations)
-print(f'|{gpw} COMBINATIONS GENERATED|')
+from generator import Generator
+
+characters =  ['0', '1'] #characters that used in the each combinations
+DIGITS = 16 # length of each combinations
+
+missed, combinations = Generator(characters, digits=DIGITS)
+gc = len(combinations)
+print(f'|{gc} COMBINATIONS GENERATED|')
 for count, combination in enumerate(combinations):
   if (count % 5) == 0:print()
   if count+1 > 100:
-    print('Not The End')
+    print('Preview ended')
     break
   print(combination, end=' ')
 if missed != 0:
   if missed > 1:print(f'\n[{missed} COMBINATIONS MISSED]')
   elif missed == 1:print('\n[1 COMBINATION MISSED]')
 else:print('\n[NO COMBINATIONS MISSED]')
-print(f'\n{(gpw/(missed+gpw))*100}% of {DIGITS} digit combinations generated')
-#wordListFILE.close()
+print(f'\n{(gc/(missed+gc))*100}% of {DIGITS} digit combinations generated')
